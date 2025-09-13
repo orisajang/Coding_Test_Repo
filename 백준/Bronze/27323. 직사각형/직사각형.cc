c182372c65e9813017f073acf97ad4e2aaffa65e
orisajang <white928620@gmail.com>
2025-09-13 17:31:45 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width = 0;
+    int height = 0;
+    
+    cin >> width >> height;
+    
+    cout << width * height;
+    
+}
