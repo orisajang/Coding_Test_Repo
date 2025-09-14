0d6d3e7273c1db463498d4005cbd2feca031fe30
orisajang <white928620@gmail.com>
2025-09-14 17:14:07 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    //f(n) = n;
+    //f(n) = 1 * n^1 이므로 1
+    cout << n << "\n";
+    cout << 1;
+    
+}
