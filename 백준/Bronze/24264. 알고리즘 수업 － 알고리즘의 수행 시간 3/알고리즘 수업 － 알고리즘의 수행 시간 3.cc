61ad5fe08c6c15e2ca6161a530d2640538a9859a
orisajang <white928620@gmail.com>
2025-09-14 17:21:01 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    //f(n) = n*n; 
+    //f(n) = 1 * n^2;  최고차항은 2
+    
+    cout << n*n << "\n";
+    cout << 2;
+}
