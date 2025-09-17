ddd933b49e21fb72c11b9180ee012051a267c978
orisajang <white928620@gmail.com>
2025-09-17 23:50:02 +0900

    [Silver IV] Title: 덱 2, Time: 160 ms, Memory: 6304 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.cc
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+    deque<int> deq;
+    for(int i=0; i< N; i++)
+    {
+        int command;
+        cin >> command;
+        int number= 0;
+        switch(command)
+        {
+            case 1:
+                cin >> number;
+                deq.push_front(number);
+                break;
+            case 2:
+                cin >> number;
+                deq.push_back(number);
+                break;
+            case 3:
+                if(deq.empty()) cout << "-1" << "\n";
+                else 
+                {
+                    cout << deq.front() << "\n";
+                    deq.pop_front();
+                }
+                break;
+            case 4:
+                if(deq.empty()) cout << "-1" << "\n";
+                else 
+                {
+                    cout<< deq.back() << "\n";
+                    deq.pop_back();
+                }
+                break;
+            case 5:
+                cout << deq.size() << "\n";
+                break;
+            case 6:
+                if(deq.empty()) cout << "1" << "\n";
+                else cout <<"0" << "\n";
+                break;
+            case 7:
+                if(deq.empty()) cout << "-1" << "\n";
+                else cout << deq.front() << "\n";
+                break;
+            case 8:
+                if(deq.empty()) cout << "-1" << "\n";
+                else cout << deq.back() << "\n";
+                break;
+        }
+        
+    }
+}
