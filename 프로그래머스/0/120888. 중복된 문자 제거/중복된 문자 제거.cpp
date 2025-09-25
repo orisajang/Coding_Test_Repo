7c3f1a616ef533ae404b3843d22c189ecaad88be
orisajang <white928620@gmail.com>
2025-09-26 01:43:15 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    int maxSize = 'z' - 'A' +1; // 10 - 5 = 5개
+    int array[1000] = {0};
+    
+    for(int i=0; i< my_string.length(); i++)
+    {
+        int diffNumber = 'z' - my_string[i];
+        if(array[diffNumber] == 0)
+        {
+            array[diffNumber] = 1;
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
