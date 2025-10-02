af8f4f256b242e5ad9e87d5a25dad0c0bbb6eef6
orisajang <white928620@gmail.com>
2025-10-02 14:29:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.61 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
@@ -4,16 +4,17 @@ public class Solution {
     public string solution(string my_string, int s, int e) {
         string answer = "";
         char[] chars = my_string.ToCharArray();
-        int index = 0;
-
-        for(int i=s; i<= e; i++)
-        {
-            char c = my_string[e - index];
-            chars[i] = c;
-            index++;
+        //1. swap으로 푸는방법 s++, e--;
+        while(s < e)
+        {   
+            char buf = chars[s];
+            chars[s] = chars[e];
+            chars[e] = buf;
+            s++;
+            e--;
         }
-        answer = new string(chars);
         
+        answer = new string(chars);
         return answer;
     }
 }
