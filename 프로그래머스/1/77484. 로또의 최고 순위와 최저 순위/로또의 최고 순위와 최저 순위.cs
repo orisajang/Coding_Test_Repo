92132a15741207f042cb39862b410a1a0b7a767e
orisajang <white928620@gmail.com>
2025-10-13 17:37:38 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.65 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -1,42 +1,31 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int[] grade = new int[7] {
-          6,6,5,4,3,2,1  
-        };
-        int count = 0;
-        for(int i=0; i< lottos.Length; i++) //로또번호 하나씩 확인
+        
+        int max = 0;
+        int min = 0;
+        foreach(var item in lottos)
         {
-            if(lottos[i] == 0) continue;
-            for(int j=0; j<win_nums.Length; j++)
+            if(item == 0)
             {
-                if(lottos[i] == win_nums[j])
-                {
-                    win_nums[j] = -1; //한번 쓴 번호니까 -1로 바꿈
-                    count++;
-                    break;
-                }
+                max++;
+            }
+            else if(win_nums.Contains(item)) //Linq 사용
+            {
+                max++;
+                min++;
             }
         }
-        answer[1] = grade[count];
-        //0인것들 예외처리
-        for(int i=0; i< lottos.Length; i++) //로또번호 하나씩 확인
-        {
-            if(lottos[i] != 0) continue; //0인거만 확인
-            for(int j=0; j<win_nums.Length; j++)
-            {
-                if(win_nums[j] != -1)
-                {
-                    win_nums[j] = -1;
-                    count++;
-                    break;
-                }
-            }
-        }
-        answer[0] = grade[count];
+        //4개 번호 일치 3등, 2개번호일치 5등 
+        //max = 4, min = 2;
+        max = 7-max > 6 ? 6 : 7-max;
+        min = 7-min > 6 ? 6 : 7-min;
+        answer[0] = max;
+        answer[1] = min;
+        
         return answer;
     }
 }
