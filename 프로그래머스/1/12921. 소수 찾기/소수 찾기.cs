e17ad30099173daba917c8cdf603b327a9192f1a
orisajang <white928620@gmail.com>
2025-10-13 15:53:57 +0900

    [level 1] Title: 소수 찾기, Time: 252.56 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -1,9 +1,7 @@
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        //소수는 제곱근까지 for문 돌리면 된다고한다(일단 처음시도는 전체for문으로 진행)
-        
-        
+        //소수는 제곱근까지 for문 돌리면 됨
         for(int i=2; i<= n; i++)
         {
             bool isSosu = true;
