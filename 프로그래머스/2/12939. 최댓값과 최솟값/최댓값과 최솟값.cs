77a4aec035d641b125b426c6377af1f9edb28137
orisajang <white928620@gmail.com>
2025-10-22 22:47:48 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 1.46 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
@@ -1,39 +1,46 @@
-
 using System;
 using System.Collections.Generic;
+using System.Text;
 public class Solution {
-
-    int FindMax(int a, int b)
-    {
-        return (a>b) ? a : b; 
-    }
-    int FindMin(int a, int b)
-    {
-        return (a<b) ? a : b; 
-    }
+    
+	int FindMax(int a, int b)
+	{
+		return (a>b) ? a : b; 
+	}
+	int FindMin(int a, int b)
+	{
+		return (a<b) ? a : b; 
+	}
     public string solution(string s) 
     {
         string answer = "";
-
-        List<int> numList = new List<int>(); 
-        string str = "";    //stringBuilder로 바꿔보기
+        
+	    List<int> numList = new List<int>(); 
+        //string str = "";    //stringBuilder로 바꿔보기
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.Length; i++)
         {
             if(s[i] == ' ') //공백을 만나면 반환
             {
-                int numBuf = int.Parse(str);
+                string strBuf = sb.ToString();
+                int numBuf = int.Parse(strBuf);
                 numList.Add(numBuf);
-                str = "";
+                //str = "";
+                sb.Clear();
             } 
             else{
-                str+= s[i];
+                //str+= s[i];
+                sb.Append(s[i]);
             }
         }
-        if(str != "") //마지막 처리,str에 뭔가 남아있으면 그거를 변환
+        //if(str != "") //마지막 처리,str에 뭔가 남아있으면 그거를 변환
+        if(sb.Length != 0)
         {
-            int buf2 = int.Parse(str);
+            string strBuf = sb.ToString();
+            int buf2 = int.Parse(strBuf);
             numList.Add(buf2);
-            str = "";
+            //str = "";
+            sb.Clear();
         }
         // 최소 최대값 변환을 함
         int max = numList[0];
@@ -44,7 +51,7 @@ public class Solution {
             min = FindMin(min, numList[i]);
         }
         answer = min + " " + max;
-
+        
         return answer;
     }
 }
