425b1f3fcb1ce755658dd609b1a3659f08a29958
orisajang <white928620@gmail.com>
2025-11-04 22:43:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 274.23 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    
+    public string rotate(string str, int index)
+    {
+        string msg = "";
+        char[] ch = str.ToCharArray();
+        for(int i=index; i<str.Length; i++)
+        {
+            msg += ch[i];
+        }
+        for(int i=0; i< index; i++)
+        {
+            msg += ch[i];
+        }
+        return msg;
+    }
+    
+    public int solution(string s) {
+        int answer = 0;
+        string str;
+        
+        Stack<char> st = new Stack<char>();
+        for(int i=0; i< s.Length; i++)
+        {
+            str = rotate(s,i);
+            bool isCorrect = true;
+            for(int j=0; j< str.Length; j++)
+            {
+                char c = str[j];
+                if(c == ')' || c == '}' || c == ']')
+                {
+                    if(st.Count == 0 ) 
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                    char popCh = st.Pop();
+                    if(popCh == '(' && c == ')'){}
+                    else if(popCh == '{' && c == '}'){}
+                    else if(popCh == '[' && c == ']'){}
+                    else
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    st.Push(str[j]);
+                }
+            }
+            if(st.Count > 0) isCorrect = false;
+            if(isCorrect) answer++;
+        }
+        
+        return answer;
+    }
+}
