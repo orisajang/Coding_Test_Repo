a1421c8bfb8ed1a0536a4ea927fb4930128a0ab8
orisajang <white928620@gmail.com>
2025-11-14 17:35:00 +0900

    [level 1] Title: 달리기 경주, Time: 436.18 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 public class Solution {
     public string[] solution(string[] players, string[] callings) {
-        string[] answer = new string[players.Length];
         //딕셔너리로 string과 순위 정보를 가지고있다면?
         Dictionary<string,int> dic = new Dictionary<string,int>();
+        //딕셔너리 초기화
         for(int i=0; i< players.Length; i++)
         {
             dic[players[i]] = i;
@@ -27,11 +27,6 @@ public class Solution {
             players[rank-1] = buf;
         }
         
-        for(int i=0; i< players.Length; i++)
-        {
-            answer[i] = players[i];
-        }
-        
-        return answer;
+        return players;
     }
 }
