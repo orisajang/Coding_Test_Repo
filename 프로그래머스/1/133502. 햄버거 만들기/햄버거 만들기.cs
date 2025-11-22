21ea055c3eed235d21455117323b93ce17f8efa0
orisajang <white928620@gmail.com>
2025-11-22 21:46:20 +0900

    [level 1] Title: 햄버거 만들기, Time: 23.99 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
@@ -4,34 +4,27 @@ using System.Collections.Generic;
 public class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        //빵,야채,고기,빵
-        //중간에 값을 빼야함 (스택과 큐는 뒤의 값을 빼기때문에 좀 그런듯)
-        //RemoveRange를 써보자
+        List<int> listStack = new List<int>();
         
-        //리스트에 저장
-        List<int> ingreList = new List<int>();
-        for(int i=0; i<ingredient.Length; i++)
+        foreach(int ingre in ingredient)
         {
-            ingreList.Add(ingredient[i]);
-        }
-        //for문 돌면서 조건 확인
-        for(int i=0; i<ingreList.Count -3; i++)
-        {   
-            //조건이 빵 야채 고기 빵 이므로 순서대로
-            if(ingreList[i] == 1 &&
-               ingreList[i+1] == 2 &&
-               ingreList[i+2] == 3 &&
-               ingreList[i+3] == 1)
+            listStack.Add(ingre);
+            
+            if(listStack.Count >= 4)
             {
-                answer++;
-                ingreList.RemoveRange(i,4);
-                //줄어든 인덱스만큼 i 처리
-                i = i - 3;
-                if(i<0) i=-1;
+                int n = listStack.Count;
+                if(listStack[n-4] == 1 &&
+                  listStack[n-3] == 2 &&
+                  listStack[n-2] == 3 &&
+                  listStack[n-1] == 1)
+                {
+                    answer++;
+                    listStack.RemoveRange(n-4,4);
+                }
             }
+            
         }
         
-        Console.WriteLine(answer);
         return answer;
     }
 }
