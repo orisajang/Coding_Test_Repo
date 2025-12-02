196c354d07c79d48f37b00081b3c3156335454a3
orisajang <white928620@gmail.com>
2025-12-02 23:12:09 +0900

    [level 0] Title: 주사위의 개수, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -3,8 +3,6 @@ using System;
 public class Solution {
     public int solution(int[] box, int n) {
         int answer = 0;
-        //80 /  2배
-        //3 2 2
         for(int i=0; i< box.Length; i++)
         {
             if(i==0) answer = box[i] / n;
