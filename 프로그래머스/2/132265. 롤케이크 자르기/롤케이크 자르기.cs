b633e592d99f14c7867cc4dacf4ce197a8803180
orisajang <white928620@gmail.com>
2025-12-05 08:23:42 +0900

    [level 2] Title: 롤케이크 자르기, Time: 173.57 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cs
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cs
@@ -1,42 +1,38 @@
 using System;
 using System.Collections.Generic;
-
 public class Solution {
     public int solution(int[] topping) {
-        int n = topping.Length;
-        if (n <= 1) return 0;
-
-        // 오른쪽에 있는 토핑들의 빈도수 카운트
-        Dictionary<int, int> rightCount = new Dictionary<int, int>();
-        foreach (int t in topping) {
-            if (rightCount.ContainsKey(t)) rightCount[t]++;
-            else rightCount[t] = 1;
-        }
-
-        HashSet<int> leftSet = new HashSet<int>(); // 왼쪽에 존재하는 토핑 종류
         int answer = 0;
-        int rightUnique = rightCount.Count;
-
-        for (int i = 0; i < n; i++) {
-            int cur = topping[i];
-
-            // 왼쪽에 추가
-            leftSet.Add(cur);
-
-            // 오른쪽에서 하나 제거
-            rightCount[cur]--;
-            if (rightCount[cur] == 0) {
-                rightCount.Remove(cur);
-                rightUnique--;
-            }
-
-            // 잘라지는 위치는 i까지가 왼쪽, i+1부터 오른쪽
-            // 오른쪽이 비어있으면 더 이상 분할 불가 -> (i == n-1) 시엔 검사하지 않음
-            if (i < n - 1 && leftSet.Count == rightUnique) {
-                answer++;
-            }
+        //딕셔너리 2개로 오른쪽 왼쪽사람이 가지고있는 토핑개수 비교
+        //오른쪽에 다 채운다음에 하나씩 왼쪽으로 넣어서 매번 전체 for문을 돌리지않게한다
+        Dictionary<int,int> dicLeft = new Dictionary<int,int>();
+        Dictionary<int,int> dicRight = new Dictionary<int,int>();
+        //오른쪽에 다 채워준다
+        for(int i=0; i< topping.Length; i++)
+        {
+            int curNum = topping[i];
+            if(!dicRight.ContainsKey(curNum)) dicRight[curNum] = 1;
+            else dicRight[curNum]++;
         }
-
+        //이제 하나씩 빼면서 왼쪽에 채워줌 (단 전체를 훑는게아닌 1개씩 빼주면서)
+        for(int i=0; i< topping.Length; i++)
+        {
+            //왼쪽에 1개 넣어주고 오른쪽에서는 1개 뺀다
+            int curNum = topping[i];
+            //왼쪽에 넣기
+            if(!dicLeft.ContainsKey(curNum)) dicLeft[curNum] = 1;
+            else dicLeft[curNum]++;
+            //오른쪽 하나 빼기
+            dicRight[curNum]--;
+            if(dicRight[curNum] == 0)
+            {
+                dicRight.Remove(curNum);
+            }
+            
+            if(dicLeft.Count == dicRight.Count) answer++;
+        }
+        
+        
         return answer;
     }
 }
