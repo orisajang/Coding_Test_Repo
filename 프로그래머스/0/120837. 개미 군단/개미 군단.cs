edadfdbcb499ef6a1dc14214966ec01b92ede24c
orisajang <white928620@gmail.com>
2025-12-10 03:24:19 +0900

    [level 0] Title: 개미 군단, Time: 0.17 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        answer += hp / 5;
+        if(hp % 5 != 0)
+        {
+            hp = hp % 5;
+            answer += hp /3;
+            if(hp % 3 != 0)
+            {
+                hp = hp % 3;
+                answer += hp;
+            }
+        }
+        
+        return answer;
+    }
+}
