cf8e5d1507a0a9c5261a46a61fa5545479031ace
orisajang <white928620@gmail.com>
2026-05-06 03:52:46 +0900

    [level 0] Title: 안전지대, Time: 0.33 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.cs
+++ 프로그래머스/0/120866. 안전지대/안전지대.cs
@@ -3,48 +3,35 @@ using System;
 public class Solution {
     public int solution(int[,] board) {
         int answer = 0;
+        int[] dx = new int[]{-1,-1,-1,0,0,0,1,1,1};
+        int[] dy = new int[]{-1,0,1,-1,0,1,-1,0,1};
         
-        int rowLength = board.GetLength(0);
-        int colLength = board.GetLength(1);
-        for(int row = 0; row < rowLength; row++ )
+        int length = board.GetLength(0);
+        
+        for(int x=0; x<length; x++)
         {
-            for(int col = 0; col < colLength; col++)
+            for(int y=0; y< length; y++)
             {
-                if(board[row,col] == 1) continue;
-                //0이라면 주변을 체크하자
-                if(row != 0)
+                if(board[x,y] == 1) continue;
+                bool isSafe = true;
+                for(int k=0; k < dx.Length; k++)
                 {
-                    if(board[row-1,col] == 1) continue; 
+                    int nx = dx[k] + x;
+                    int ny = dy[k] + y;
+                    
+                    if(nx >= 0 && nx < length && ny >= 0 && ny < length)
+                    {
+                        if(board[nx,ny] == 1)
+                        {
+                            isSafe = false;
+                            break;
+                        }
+                    }
                 }
-                if(col != 0)
+                if(isSafe)
                 {
-                    if(board[row,col-1] == 1) continue;
+                    answer++;    
                 }
-                if(row != 0 && col != 0)
-                {
-                    if(board[row-1,col-1] ==1) continue;
-                }
-                if(row != rowLength-1)
-                {
-                    if(board[row+1,col] == 1) continue;
-                }
-                if(col != colLength-1)
-                {
-                    if(board[row,col+1] == 1) continue;
-                }
-                if(row != rowLength-1 && col != 0)
-                {
-                    if(board[row+1,col-1] == 1) continue;
-                }
-                if(row != 0 && col != colLength-1)
-                {
-                    if(board[row-1,col+1] == 1) continue;
-                }
-                if(row != rowLength-1 && col != colLength-1)
-                {
-                    if(board[row+1,col+1] == 1) continue;
-                }
-                answer++;
             }
         }
         
