6c3434634988e78249b5e4c148edb45eb6077c06
orisajang <white928620@gmail.com>
2026-06-14 12:46:24 +0900

    [level 2] Title: 방문 길이, Time: 0.45 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cs
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cs
@@ -5,9 +5,10 @@ public class Solution {
     public int solution(string dirs) {
         int answer = 0;
         //HashSet도 가능하고, int[,,,] 이거도 가능함
-        int posX = 0;
-        int posY = 0;
-        HashSet<string> hash = new HashSet<string>();
+        //HashSet<string> hash = new HashSet<string>();
+        int[,,,] recordPos = new int[11,11,11,11];
+        int posX = 5;
+        int posY = 5;
         
         //좌표는 -5 ~ 5 이상으로 넘어갈수 없음.
         for(int i=0; i< dirs.Length; i++)
@@ -17,54 +18,32 @@ public class Solution {
             if(dirs[i] == 'U')
             {
                 //위쪽으로 이동;
-                if(ExistPos(posX, posY+1))
-                {
-                    posY += 1;
-                }
+                if(posY +1 > 10) continue;
+                posY += 1;
             }
             else if(dirs[i] == 'D')
             {
-                if(ExistPos(posX, posY-1))
-                {
-                    posY -= 1;
-                }
+                if(posY -1 < 0) continue;
+                posY -= 1;
             }
             else if(dirs[i] == 'R')
             {
-                if(ExistPos(posX+1, posY))
-                {
-                    posX += 1;
-                }
+                if(posX +1 > 10) continue;
+                posX += 1;
             }
             else if(dirs[i] == 'L')
             {
-                if(ExistPos(posX-1, posY))
-                {
-                    posX -= 1;
-                }
+                if(posX -1 < 0) continue;
+                posX -= 1;
             }
-            if(beforeX == posX && beforeY == posY) continue;
-            
             //중복으로 도달하지않았으면 answer++
-            string posStr = $"({posX},{posY}) -> ({beforeX},{beforeY})";
-            string posStr2 = $"({beforeX},{beforeY}) -> ({posX},{posY})";
-            if(!hash.Contains(posStr) && !hash.Contains(posStr2))
+            if(recordPos[beforeX,beforeY,posX,posY] == 0)
             {
-                hash.Add(posStr);
-                hash.Add(posStr2);
+                recordPos[beforeX,beforeY,posX,posY] = 1;
+                recordPos[posX,posY,beforeX,beforeY] = 1;
                 answer++;
             }
         }
         return answer;
     }
-    
-    private bool ExistPos(int posX,int posY)
-    {
-        if(posX > 5 || posY > 5 || posX < -5 || posY < -5)
-        {
-            return false;
-        }
-        return true;
-    }
-    
 }
